8609b66960d30a40ed65fbf236152ca4da6c9910
kimyujeong <yujng789@gmail.com>
2022-02-03 11:19:47 +0900

    feat: 일기장상세페이지

---
+++ front-end/src/components/DiaryDetail.vue
@@ -0,0 +1,162 @@
+<template>
+    <va-modal
+     v-model="state.detailVisible"
+     no-outside-dismiss
+     hide-default-actions
+     max-height= 100%
+     max-width= 100%
+    >
+    <div class="modal">
+        <div class="modal-header">
+            <va-button class="modal-btn" icon="clear" @click="closeDetail"></va-button>
+        </div>
+
+        <div class="modal-content">
+            <section class="mx-3 my-5 video"></section>
+            <div class="mx-3 my-5">
+                <!-- <div v-if="this.contentVisible">
+                    <va-button :rounded="false" outline class="mr-4" @click="moveContent">이전</va-button>
+                    <va-button :rounded="false" class="mr-4">완료</va-button>
+                </div>
+                <div v-else>
+                    <DiaryDetailContent @moveContent="moveContent"/>
+                </div> -->
+                <div>분석결과</div>
+                <div class="container">
+                    <DiaryDetailContent class="front" @moveContent="moveContent"/> 
+                    <DiaryDetailEmotion class="back"/> 
+                </div>
+                <va-button :rounded="false" outline class="mr-4" @click="moveEmotion">다음</va-button>
+                <va-button :rounded="false">완료</va-button>
+            </div>
+        </div>
+    </div>
+
+    </va-modal>
+
+</template>
+
+<script>
+import { computed, reactive } from 'vue'
+import DiaryDetailContent from '@/components/DiaryDetailContent.vue'
+import DiaryDetailEmotion from '@/components/DiaryDetailEmotion.vue'
+
+export default {
+    name: 'DiaryDetail',
+    props: {
+        open: {
+            type: Boolean,
+            default: false
+        },
+    },
+    components: {
+        DiaryDetailContent,
+        DiaryDetailEmotion
+    },
+    setup(props, {emit}) {
+        const state = reactive({
+            detailVisible: computed(() => props.open),
+            contentVisible: false
+        })
+        
+        const closeDetail = function() {
+            emit('closeDetail')
+        }
+
+        const moveContent = function() {
+            state.contentVisible = !state.contentVisible
+        }
+
+        const moveEmotion = function() {
+            let card = document.querySelector('.container')
+            if (card.style.transform == "rotateY(180deg)") {
+                card.style.transform = "rotateY(0deg)"
+            } else {
+                card.style.transform = "rotateY(180deg)"
+            }
+        }
+    return { state, closeDetail, moveContent, moveEmotion }
+    }
+
+}
+</script>
+
+<style>
+.modal {
+    width: 90vw;
+    height: 85vh;
+}
+
+.modal-header {
+    display: flex;
+}
+
+.modal-btn {
+    margin-left: auto;
+}
+
+.modal-content {
+    display: flex;
+    justify-content: center;
+    margin-top: 2vw;
+}
+
+.video {
+  display: block;
+  width: 45vw;
+  height: 55vh;
+  background: lightgray;
+}
+/* 
+.container { 
+    cursor: pointer; 
+    perspective: 100rem; 
+} 
+
+.card{   */
+    /*webkit을 사용해야지 사파리에서도 정상적으로 동작함*/ 
+    /* -webkit-backface-visibility: hidden; 
+    -webkit-transform: translate3d(0,0,0); 
+    -webkit-perspective: 0; 
+    -webkit-transition: 1s; 
+    backface-visibility: hidden; /*뒷면 숨기기*/ 
+    /* visibility: visible; 
+    transition: 1s; 
+}  
+
+.front { 
+    position: absolute;
+    transform: rotateY(0deg); 
+} 
+
+.back { 
+    transform: rotateY(-180deg); 
+} 
+
+.container:hover .front {  */
+    /*y축을 기준으로 180도까지회전*/ 
+    /* transform: rotateY(180deg); 
+} 
+
+.container:hover .back {  */
+    /*y축을 기준으로 0도 까지회전*/ 
+    /* transform: rotateY(0deg);
+} */
+
+.container {
+    perspective: 100rem;
+    transition: transform 1s;
+    transform-style: preserve-3d;
+    cursor: pointer;
+}
+
+.front { 
+    position: absolute;
+    transform: rotateY(0deg); 
+} 
+
+.back { 
+    transform: rotateY(-180deg); 
+} 
+
+</style>
---
+++ front-end/src/components/DiaryDetailContent.vue
@@ -0,0 +1,31 @@
+<template>
+  
+  <div class="video-content"></div>
+  
+</template>
+
+<script>
+
+export default {
+    name: "DiaryDetailContent",
+
+    setup(props, {emit}) {
+      const moveContent = function() {
+        emit('moveContent')
+      }
+
+      return {moveContent}
+    }
+}
+</script>
+
+<style>
+.video-content {
+  display: block;
+  width: 25vw;
+  height: 50vh;
+  background: lightgray;
+}
+
+
+</style>
