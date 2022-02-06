90fc18e8c3e0bb378513c92e0611c7f83fe0fd21
ethankim93 <wsmh.kim12@gmail.com>
2022-02-07 01:13:46 +0900

    feat: 원스크롤 페이지 추가

--- front-end/src/views/Home.vue
+++ front-end/src/views/Home.vue
@@ -1,7 +1,34 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <main id="fullpage">
+      <section class="section">
+        <div id="app"></div>
+          <h2>section 1</h2>
+      </section>
+      <section class="section">
+        <div class="slide">
+          <h3>slide 01</h3>
+        </div>
+        <div class="slide">
+          <h3>slide 02</h3>
+        </div>
+        <div class="slide">
+          <h3>slide 03</h3>
+        </div>
+        <h2>section 2</h2>
+      </section>
+      <section class="section">
+        <h2>section 3</h2>
+      </section>
+      <section class="section">
+        <h2>section 4</h2>
+
+      </section>
+      <footer class="section fp-auto-height">
+        <h2>footer</h2>
+
+      </footer>
+    </main>
   </div>
 </template>
 
