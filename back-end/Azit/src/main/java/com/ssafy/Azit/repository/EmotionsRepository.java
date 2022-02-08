8a748a34a79f81444f4121cc3f2efc4c664109cc
조용구 <younygo1004@naver.com>
2022-02-08 11:04:55 +0900

    style : IntelliJ 오류 해결
    - 변경사항 없음.

--- back-end/Azit/src/main/java/com/ssafy/Azit/repository/EmotionsRepository.java
+++ back-end/Azit/src/main/java/com/ssafy/Azit/repository/EmotionsRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmotionsRepository extends JpaRepository<Emotions, Long> {
+public interface EmotionsRepository extends JpaRepository<Emotions, Long>{
 
 }
