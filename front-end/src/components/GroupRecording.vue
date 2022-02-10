46e6a01a3adeb282342c171c27b0d19ad38dc59a
오은진 <oeunj08@naver.com>
2022-02-10 11:36:13 +0900

    style : 코드 의미 없는 수정

---
+++ front-end/src/components/GroupRecording.vue
@@ -0,0 +1,190 @@
+<template>
+  <va-modal
+    v-model="state.recordingVisible"
+    no-outside-dismiss
+    hide-default-actions
+    max-height="100%"
+    max-width="100%"
+  >
+    <div class="recording-modal">
+      <div class="modal-header">
+        <va-button
+          class="modal-btn"
+          color="#6565ca"
+          icon="clear"
+          @click="closeRecording"
+        ></va-button>
+      </div>
+
+      <!-- 영상 출력 -->
+      <div id="session" v-if="session">
+        <!-- session 헤더 및 leaveSession 버튼 -->
+        <!-- <div id="session-header">
+          <input
+            class="btn btn-large btn-danger"
+            type="button"
+            id="buttonLeaveSession"
+            @click="leaveSession"
+            value="Leave session"
+          />
+        </div> -->
+        <div id="main-video" class="flex md4">
+          <user-video :stream-manager="mainStreamManager" />
+        </div>
+        <div id="video-container" class="flex md4">
+          <user-video :stream-manager="publisher" />
+          <!-- @click.native="updateMainVideoStreamManager(publisher)" -->
+          <user-video
+            v-for="sub in subscribers"
+            :key="sub.stream.connection.connectionId"
+            :stream-manager="sub"
+          />
+          <!-- @click.native="updateMainVideoStreamManager(sub)" -->
+        </div>
+      </div>
+    </div>
+  </va-modal>
+</template>
+
+<script>
+import { computed, reactive } from "vue";
+import UserVideo from "./UserVideo.vue";
+
+export default {
+  components: {
+    UserVideo,
+  },
+  name: "GroupRecording",
+  props: {
+    open: {
+      type: Boolean,
+      default: false,
+    },
+    session: {
+      type: Object,
+    },
+    mainStreamManager: {
+      type: Object,
+    },
+    publisher: {
+      type: Object,
+    },
+  },
+  setup(props, { emit }) {
+    const state = reactive({
+      recordingVisible: computed(() => props.open),
+    });
+
+    const closeRecording = function () {
+      emit("closeRecording");
+    };
+    return { state, closeRecording };
+  },
+};
+</script>
+
+<style scoped>
+.recording-modal {
+  width: 90vw;
+  height: 85vh;
+}
+
+.modal-header {
+  display: flex;
+}
+
+.modal-btn {
+  margin-left: auto;
+}
+
+#session-header {
+  margin-bottom: 20px;
+}
+
+#session-title {
+  display: inline-block;
+}
+
+#buttonLeaveSession {
+  float: right;
+  margin-top: 20px;
+}
+
+#session img {
+  width: 100%;
+  height: auto;
+  display: inline-block;
+  object-fit: contain;
+  vertical-align: baseline;
+}
+
+#session #video-container img {
+  position: relative;
+  float: left;
+  width: 50%;
+  cursor: pointer;
+  object-fit: cover;
+  height: 180px;
+}
+
+#video-container video {
+  position: relative;
+  float: left;
+  width: 50%;
+  cursor: pointer;
+}
+
+#video-container video + div {
+  float: left;
+  width: 50%;
+  position: relative;
+  margin-left: -50%;
+}
+
+#video-container p {
+  display: inline-block;
+  background: #f8f8f8;
+  padding-left: 5px;
+  padding-right: 5px;
+  color: #777777;
+  font-weight: bold;
+  border-bottom-right-radius: 4px;
+}
+
+video {
+  width: 100%;
+  height: auto;
+}
+
+#main-video p {
+  position: absolute;
+  display: inline-block;
+  background: #f8f8f8;
+  padding-left: 5px;
+  padding-right: 5px;
+  font-size: 22px;
+  color: #777777;
+  font-weight: bold;
+  border-bottom-right-radius: 4px;
+}
+
+input.btn {
+  font-weight: bold;
+}
+
+.btn {
+  font-weight: bold !important;
+}
+
+.btn-success {
+  background-color: #06d362 !important;
+  border-color: #06d362;
+}
+
+.btn-success:hover {
+  background-color: #1abd61 !important;
+  border-color: #1abd61;
+}
+</style>
+
+
--- front-end/src/components/Recording.vue
+++ front-end/src/components/Recording.vue
@@ -1,55 +1,59 @@
 <template>
-    <va-modal
-     v-model="state.recordingVisible"
-     no-outside-dismiss
-     hide-default-actions
-     max-height= 100%
-     max-width= 100%
-    >
+  <va-modal
+    v-model="state.recordingVisible"
+    no-outside-dismiss
+    hide-default-actions
+    max-height="100%"
+    max-width="100%"
+  >
     <div class="recording-modal">
-        <div class="modal-header">
-            <va-button class="modal-btn" color=#6565ca icon="clear" @click="closeRecording"></va-button>
-        </div>
+      <div class="modal-header">
+        <va-button
+          class="modal-btn"
+          color="#6565ca"
+          icon="clear"
+          @click="closeRecording"
+        ></va-button>
+      </div>
     </div>
-
-    </va-modal>
+  </va-modal>
 </template>
 
 <script>
-import { computed, reactive } from 'vue'
+import { computed, reactive } from "vue";
 
 export default {
-    name: 'Recording',
-    props: {
-        open: {
-            type: Boolean,
-            default: false
-        },
+  name: "Recording",
+  props: {
+    open: {
+      type: Boolean,
+      default: false,
     },
-    setup(props, {emit}) {
-        const state = reactive({
-            recordingVisible: computed(() => props.open)
-        })
+  },
+  setup(props, { emit }) {
+    const state = reactive({
+      recordingVisible: computed(() => props.open),
+    });
 
-        const closeRecording = function() {
-            emit('closeRecording')
-        }
-        return { state, closeRecording }
-    }
-}
+    const closeRecording = function () {
+      emit("closeRecording");
+    };
+    return { state, closeRecording };
+  },
+};
 </script>
 
 <style scoped>
 .recording-modal {
-    width: 90vw;
-    height: 85vh;
+  width: 90vw;
+  height: 85vh;
 }
 
 .modal-header {
-    display: flex;
+  display: flex;
 }
 
 .modal-btn {
-    margin-left: auto;
+  margin-left: auto;
 }
 </style>
