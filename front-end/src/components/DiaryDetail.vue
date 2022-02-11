f2c80e7a573e76a9816be3013c6b4ddf13f3a07e
장성태 <jst1365@nate.com>
2022-02-11 10:38:26 +0900

    <refactor>: 프론트 1차 코드 병합

--- front-end/src/components/DiaryDetail.vue
+++ front-end/src/components/DiaryDetail.vue
@@ -1,101 +1,105 @@
 <template>
-    <va-modal
-     v-model="state.detailVisible"
-     no-outside-dismiss
-     hide-default-actions
-     max-height= 100%
-     max-width= 100%
-    >
+  <va-modal
+    v-model="state.detailVisible"
+    no-outside-dismiss
+    hide-default-actions
+    max-height="100%"
+    max-width="100%"
+  >
     <div class="modal">
-        <div class="modal-header">
-            <va-button class="modal-btn" color=#6565ca icon="clear" @click="closeDetail"></va-button>
-        </div>
+      <div class="modal-header">
+        <va-button
+          class="modal-btn"
+          color="#6565ca"
+          icon="clear"
+          @click="closeDetail"
+        ></va-button>
+      </div>
 
-        <div class="modal-content">
-            <section class="mx-3 my-5 video"></section>
-            <div class="mx-3 my-5">
-                
-                <div class="detail-container">
-                    <DiaryDetailContent class="front" @moveContent="moveContent"/>
-                    <DiaryDetailEmotion class="back"/> 
-                </div>
-
-            </div>
+      <div class="modal-content">
+        <section class="mx-3 my-5 video" @click="moveRecordingVideo"></section>
+        <div class="mx-3 my-5">
+          <div class="detail-container">
+            <DiaryDetailContent class="front" @moveContent="moveContent" />
+            <DiaryDetailEmotion class="back" />
+          </div>
         </div>
+      </div>
     </div>
-
-    </va-modal>
-
+  </va-modal>
 </template>
 
 <script>
-import { computed, reactive } from 'vue'
-import DiaryDetailContent from '@/components/DiaryDetailContent.vue'
-import DiaryDetailEmotion from '@/components/DiaryDetailEmotion.vue'
-import Buttons from './Buttons.vue'
-import WhiteButtons from './WhiteButtons.vue'
+import { computed, reactive } from "vue";
+import DiaryDetailContent from "@/components/DiaryDetailContent.vue";
+import DiaryDetailEmotion from "@/components/DiaryDetailEmotion.vue";
 
 export default {
-    name: 'DiaryDetail',
-    props: {
-        open: {
-            type: Boolean,
-            default: false
-        },
+  name: "DiaryDetail",
+  props: {
+    move: {
+      type: Boolean,
+      default: false,
     },
-    components: {
-        DiaryDetailContent,
-        DiaryDetailEmotion,
-        Buttons,
-        WhiteButtons
+    recordingUrl: {
+      type: String,
     },
-    setup(props, {emit}) {
-        const state = reactive({
-            detailVisible: computed(() => props.open),
-            contentVisible: false
-        })
-        
-        const closeDetail = function() {
-            emit('closeDetail')
-        }
+  },
+  components: {
+    DiaryDetailContent,
+    DiaryDetailEmotion,
+  },
 
-        const moveContent = function() {
-            state.contentVisible = !state.contentVisible
-        }
+  setup(props, { emit }) {
+    const state = reactive({
+      detailVisible: computed(() => props.move),
+      contentVisible: false,
+    });
 
-        const moveEmotion = function() {
-            let card = document.querySelector('.detail-container')
-            if (card.style.transform == "rotateY(180deg)") {
-                card.style.transform = "rotateY(0deg)"
-            } else {
-                card.style.transform = "rotateY(180deg)"
-            }
-        }
-    return { state, closeDetail, moveContent, moveEmotion }
-    }
+    const closeDetail = function () {
+      emit("closeDetail");
+    };
 
-}
+    const moveContent = function () {
+      state.contentVisible = !state.contentVisible;
+    };
+
+    const moveRecordingVideo = function () {
+      console.log(this.recordingUrl);
+      window.open(this.recordingUrl, "_blank");
+    };
+
+    const moveEmotion = function () {
+      let card = document.querySelector(".detail-container");
+      if (card.style.transform == "rotateY(180deg)") {
+        card.style.transform = "rotateY(0deg)";
+      } else {
+        card.style.transform = "rotateY(180deg)";
+      }
+    };
+    return { state, closeDetail, moveContent, moveEmotion, moveRecordingVideo };
+  },
+};
 </script>
 
 <style scoped>
 .modal {
-    width: 90vw;
-    height: 85vh;
+  width: 90vw;
+  height: 85vh;
 }
 
 .modal-header {
-    display: flex;
+  display: flex;
 }
 
 .modal-btn {
-    margin-left: auto;
+  margin-left: auto;
 }
 
 .modal-content {
-    display: flex;
-    justify-content: center;
-    margin-top: 1vw;
-    
+  display: flex;
+  justify-content: center;
+  margin-top: 1vw;
 }
 
 .video {
@@ -111,13 +115,13 @@ export default {
 } 
 
 .card{   */
-    /*webkit을 사용해야지 사파리에서도 정상적으로 동작함*/ 
-    /* -webkit-backface-visibility: hidden; 
+/*webkit을 사용해야지 사파리에서도 정상적으로 동작함*/
+/* -webkit-backface-visibility: hidden; 
     -webkit-transform: translate3d(0,0,0); 
     -webkit-perspective: 0; 
     -webkit-transition: 1s; 
-    backface-visibility: hidden; /*뒷면 숨기기*/ 
-    /* visibility: visible; 
+    backface-visibility: hidden; /*뒷면 숨기기*/
+/* visibility: visible; 
     transition: 1s; 
 }  
 
@@ -131,36 +135,34 @@ export default {
 } 
 
 .container:hover .front {  */
-    /*y축을 기준으로 180도까지회전*/ 
-    /* transform: rotateY(180deg); 
+/*y축을 기준으로 180도까지회전*/
+/* transform: rotateY(180deg); 
 } 
 
 .container:hover .back {  */
-    /*y축을 기준으로 0도 까지회전*/ 
-    /* transform: rotateY(0deg);
+/*y축을 기준으로 0도 까지회전*/
+/* transform: rotateY(0deg);
 } */
 
 .detail-container {
-    perspective: 100rem;
-    transition: transform 1s;
-    transform-style: preserve-3d;
-    cursor: pointer;
-    background: #ffffff;
+  perspective: 100rem;
+  transition: transform 1s;
+  transform-style: preserve-3d;
+  cursor: pointer;
+  background: #ffffff;
 }
 
-.front { 
-    position: absolute;
-    transform: rotateY(0deg); 
-} 
+.front {
+  position: absolute;
+  transform: rotateY(0deg);
+}
 
-.back { 
-    
-    transform: rotateY(-180deg); 
-} 
+.back {
+  transform: rotateY(-180deg);
+}
 
 .detail-btns {
-    display: flex;
-    justify-content: flex-end;
+  display: flex;
+  justify-content: flex-end;
 }
-
 </style>
--- front-end/src/components/DiaryListItem.vue
+++ front-end/src/components/DiaryListItem.vue
@@ -1,46 +1,45 @@
 <template>
   <va-card @click="onOpenDetail">
-    <va-image
-      src="https://picsum.photos/400/200"
-      style="height: 200px;"
-    />
+    <va-image src="https://picsum.photos/400/200" style="height: 200px" />
     <va-card-title>Title</va-card-title>
-    <va-card-content>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</va-card-content>
+    <va-card-content
+      >Lorem ipsum dolor sit amet, consectetur adipiscing elit.</va-card-content
+    >
   </va-card>
-  <DiaryDetail 
+  <DiaryDetail
     :open="openDetail"
-    @closeDetail="this.openDetail = !this.openDetail"/>
+    @closeDetail="this.openDetail = !this.openDetail"
+  />
 </template>
 
 <script>
-import axios from 'axios'
-import DiaryDetail from '@/components/DiaryDetail.vue'
+import axios from "axios";
+import DiaryDetail from "@/components/DiaryDetail.vue";
 
 export default {
-    name:'DiaryListItem',
-    components: {
-      DiaryDetail
-    },
-    data(){
-      return{
-        openDetail: false,
-      };
-    },
-    methods:{
-    onOpenDetail () {
-      this.openDetail = !this.openDetail
+  name: "DiaryListItem",
+  components: {
+    DiaryDetail,
+  },
+  data() {
+    return {
+      openDetail: false,
+    };
+  },
+  methods: {
+    onOpenDetail() {
+      this.openDetail = !this.openDetail;
       axios({
-        method: 'get',
-        url: 'http://localhost:8080/v1/diary/{diaryid}'
-      }).catch( res => {
-        console.log(res)
+        method: "get",
+        url: "http://localhost:8080/v1/diary/{diaryid}",
+      }).catch((res) => {
+        console.log(res);
         // 받아와서 store에 저장 -> store에서 가져와서 디테일 페이지
-      })
-    }
-  }
-}
+      });
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
