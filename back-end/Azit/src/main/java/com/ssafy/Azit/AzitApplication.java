fd618749b54aaed8e39d76592dc3fa575fcfb03c
오은진 <oeunj08@naver.com>
2022-02-11 13:35:52 +0900

    chore : 주석 한글처리

--- back-end/Azit/src/main/java/com/ssafy/Azit/AzitApplication.java
+++ back-end/Azit/src/main/java/com/ssafy/Azit/AzitApplication.java
@@ -1,6 +1,5 @@
 package com.ssafy.Azit;
 
-import com.ssafy.Azit.controller.SampleController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
