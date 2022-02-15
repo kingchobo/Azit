fe7effe17287fc80a88cc1c99592a5abee5701b8
kingchobo <kingchobo>
2022-02-15 15:05:00 +0900

    fix : 일기장 페이지 썸네일 수정

--- front-end/src/components/DiaryListItem.vue
+++ front-end/src/components/DiaryListItem.vue
@@ -1,22 +1,27 @@
 <template>
-  <div class="flex md3">
-    <va-card @click="onOpenDetail" class="item" style="margin: 0.75rem">
-      <va-image :src="diary.thumbNail" style="height: 200px" />
+    <div class="flex md3">
+        <va-card @click="onOpenDetail" class="item" style="margin: 0.75rem">
+            <!-- <va-image :src="diary.thumbNail" style="height: 200px" /> -->
+            <video
+                :src="diary.videoLink"
+                preload="metadata"
+                style="height: 200px"
+            />
 
-      <!-- <va-image
+            <!-- <va-image
         src="https://picsum.photos/400/200"
         style="height: 200px;"
       /> -->
-      <!-- <va-card-title>{{diary.title}}</va-card-title> -->
-      <va-card-content>제목 : {{ diary.title }}</va-card-content>
-      <va-card-content>작성일 : {{ diary.createDate }}</va-card-content>
-    </va-card>
-  </div>
-  <DiaryDetail
-    :move="openDetail"
-    :diaryContent="diary"
-    @closeDetail="this.openDetail = !this.openDetail"
-  />
+            <!-- <va-card-title>{{diary.title}}</va-card-title> -->
+            <va-card-content>제목 : {{ diary.title }}</va-card-content>
+            <va-card-content>작성일 : {{ diary.createDate }}</va-card-content>
+        </va-card>
+    </div>
+    <DiaryDetail
+        :move="openDetail"
+        :diaryContent="diary"
+        @closeDetail="this.openDetail = !this.openDetail"
+    />
 </template>
 
 <script>
@@ -24,37 +29,36 @@ import DiaryDetail from "@/components/DiaryDetail.vue";
 import axios from "axios";
 
 export default {
-  name: "DiaryListItem",
-  props: {
-    diary: {
-      type: Object,
+    name: "DiaryListItem",
+    props: {
+        diary: {
+            type: Object,
+        },
     },
-  },
-  components: {
-    DiaryDetail,
-  },
-  data() {
-    return {
-      openDetail: false,
-      diaryContent: [],
-    };
-  },
-  methods: {
-    onOpenDetail() {
-      this.openDetail = !this.openDetail;
-      console.log(this.diary);
-      // axios.get('https://045d5080-b0f3-4dd5-9240-aee771955f6d.mock.pstmn.io/diarydetail')
-      // axios
-      //   .get(
-      //     `https://045d5080-b0f3-4dd5-9240-aee771955f6d.mock.pstmn.io/diary/${this.diary.diaryId}`
-      //   )
-      //   .then((response) => {
-      //     this.diaryContent = response.data;
-      //   });
+    components: {
+        DiaryDetail,
     },
-  },
+    data() {
+        return {
+            openDetail: false,
+            diaryContent: [],
+        };
+    },
+    methods: {
+        onOpenDetail() {
+            this.openDetail = !this.openDetail;
+            console.log(this.diary);
+            // axios.get('https://045d5080-b0f3-4dd5-9240-aee771955f6d.mock.pstmn.io/diarydetail')
+            // axios
+            //   .get(
+            //     `https://045d5080-b0f3-4dd5-9240-aee771955f6d.mock.pstmn.io/diary/${this.diary.diaryId}`
+            //   )
+            //   .then((response) => {
+            //     this.diaryContent = response.data;
+            //   });
+        },
+    },
 };
 </script>
 
-<style>
-</style>
+<style></style>
