cba1ef941a8834a5c104c9d5264f729b4fe839fd
조용구 <younygo1004@naver.com>
2022-02-17 00:16:25 +0900

    fix : 일기 상세정보 부분 수정
    - 일기장 상세내역 보기 디자인 변경
    - 그룹일기에 참여한 구성원 일기 모두 불러오도록 변경
    - 자신의 일기 내용 수정 가능하도록 변경

--- front-end/src/components/DiaryDetailText.vue
+++ front-end/src/components/DiaryDetailText.vue
@@ -1,23 +1,42 @@
 <template>
     <div class="video-content">
-        gdfgdf
         <div class="main-container">
-            sdafasdgf
-            <div class="text-block">
+            <div class="text-container background-none">
                 <p class="display-3">
                     {{ this.diaryContentDetail.title }}
                 </p>
             </div>
-            <!-- v-show="state.editVisible" -->
-            <div class="text-block group-text-area">
-                sadgsfagdfgdf
-                <!-- <p v-html="state.diaryGroupText"></p> -->
+            <!--  -->
+            <div
+                v-show="state.editVisible"
+                class="text-block group-text-area background-none"
+            >
+                <p v-html="state.diaryGroupText"></p>
             </div>
+
+            <va-input
+                v-show="state.updateVisible"
+                class="mt-4"
+                v-model="state.diaryMyText"
+                type="textarea"
+                :max-rows="11"
+            />
+
             <div class="row justify--center">
-                <!-- v-show="state.editVisible" -->
-                <Buttons class="mx-2" btn-text="내용 수정" @click="editDiary" />
-                <!-- v-show="state.updateVisible" -->
-                <Buttons class="mx-2" btn-text="내용 저장" @click="saveDiary" />
+                <!--  -->
+                <Buttons
+                    v-show="state.editVisible"
+                    class="mx-2"
+                    btn-text="내용 수정"
+                    @click="editDiary"
+                />
+                <!--  -->
+                <Buttons
+                    v-show="state.updateVisible"
+                    class="mx-2"
+                    btn-text="내용 저장"
+                    @click="saveDiary"
+                />
             </div>
         </div>
     </div>
@@ -41,7 +60,7 @@ export default {
     // data: {
     //     thisTest: "this 테스트용 데이터",
     // },
-    async setup(props) {
+    setup(props) {
         const state = reactive({
             // EditAvaliable: true,
             editVisible: true,
@@ -62,34 +81,39 @@ export default {
         //   }
         // };
 
-        axios
-            .get(`/api/diary/group/${props.diaryContentDetail.diaryId}`)
-            .then(({ data: diaryList }) => {
-                diaryList.forEach(async (diaryObject) => {
-                    console.log(diaryObject); // 일기 객체
-                    await axios
-                        .get(`/api/user/${diaryObject.user.userId}`)
-                        .then(({ data: userObj }) => {
-                            console.log(props.diaryContentDetail);
-                            console.log(userObj.name);
-                            state.diaryGroupText += `${userObj.name}`;
-                        });
-                    state.diaryGroupText += `${diaryObject.content}`;
-                    console.log(state.diaryGroupText);
+        const getGroupDiaryContent = () => {
+            state.diaryGroupText = "";
+            axios
+                .get(`/api/diary/group/${props.diaryContentDetail.diaryId}`)
+                .then(({ data: diaryList }) => {
+                    diaryList.forEach(async (diaryObject) => {
+                        console.log(diaryObject); // 일기 객체
+                        await axios
+                            .get(`/api/user/${diaryObject.user.userId}`)
+                            .then(({ data: userObj }) => {
+                                console.log(props.diaryContentDetail);
+                                console.log(userObj.name);
+                                state.diaryGroupText += `<p style="padding:5px; font-size: 1.5em;">${userObj.name}<p>`;
+                            });
+                        state.diaryGroupText += `${diaryObject.content} <br /> <br />`;
+                        console.log(state.diaryGroupText);
+                    });
                 });
-            });
+        };
+
+        getGroupDiaryContent();
 
         const editDiary = function () {
             state.editVisible = false;
             state.updateVisible = true;
         };
 
-        const saveDiary = function () {
+        const saveDiary = async () => {
             const newDiary = props.diaryContentDetail;
 
             newDiary.content = state.diaryMyText;
 
-            axios
+            await axios
                 .put(
                     `api/diary?diaryid=${props.diaryContentDetail.diaryId}`,
                     newDiary
@@ -97,6 +121,8 @@ export default {
                 .then((response) => {
                     console.log(response);
                 });
+
+            getGroupDiaryContent();
             state.editVisible = true;
             state.updateVisible = false;
         };
@@ -143,12 +169,12 @@ export default {
 }
 
 .background-none {
-    /* background-color: transparent; */
+    background-color: transparent;
 }
 
-/* .group-text-area {
+.group-text-area {
     height: 50%;
     overflow-y: scroll;
     font-size: 1.2em;
-} */
+}
 </style>
